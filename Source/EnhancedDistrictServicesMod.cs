bb7f1b324a9d32632744ac5f7afab70c0365afd6
Paul Chang <paulacchang@live.com>
2019-12-01 20:33:28 -0800

    Fix for too much outside connections traffic ..

--- Source/EnhancedDistrictServicesMod.cs
+++ Source/EnhancedDistrictServicesMod.cs
@@ -9,8 +9,8 @@ namespace EnhancedDistrictServices
     /// </summary>
     public class EnhancedDistrictServicesMod : IUserMod, ILoadingExtension
     {
-        public const string version = "1.0.8";
-        public string Name => "EnhancedDistrictServices 1.0.8";
+        public const string version = "1.0.9";
+        public string Name => "EnhancedDistrictServices 1.0.9";
         public string Description => "EnhancedDistrictServices mod for Cities Skylines, which allows more granular control of services and supply chains.";
 
         public void OnCreated(ILoading loading)
